459b98f754245b9cb4884f7be08ed81988f7b765
Joe Fletcher <joe@fletcherdigital.com>
2019-07-19 07:26:17 -0400

    Create fd-disable-local-plugins.php

---
+++ fd-disable-local-plugins.php
@@ -0,0 +1,25 @@
+<?php
+/*
+Plugin Name: FD Disable Local Plugins
+Plugin URI: https://github.com/heyfletch/fd-disable-local-plugins
+Description:  Disables plugins based on local config.
+Author: Joe Fletcher
+Author URI: https://fletcherdigital.com
+Credit: Kamil Grzegorczyk for v0.1
+Version: 0.2
+*/
+
+if (defined('LOCAL_DISABLED_PLUGINS')) {
+  $plugins_to_disable = unserialize(LOCAL_DISABLED_PLUGINS);
+
+  if (!empty($plugins_to_disable) && is_array($plugins_to_disable)) {
+
+    // Use Mark Jaquith code
+    require_once(dirname(__FILE__) . '/vendor/DisablePlugins.php');
+
+    $utility = new DisablePlugins($plugins_to_disable);
+
+    // Optional
+    error_log('Locally disabled plugins: ' . var_export($plugins_to_disable, true));
+  }
+}
